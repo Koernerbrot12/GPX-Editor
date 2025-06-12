e5f834cdb3986c1946cf321de7d11afa2ab08335
Koernerbrot <felixkoerner3@gmail.com>
2025-06-12 17:55:06 +0200

    modified:   Waypoints.py 	modified:   __pycache__/Waypoints.cpython-311.pyc 	new file:   __pycache__/main.cpython-311.pyc 	modified:   __pycache__/menu.cpython-311.pyc 	modified:   __pycache__/parser.cpython-311.pyc 	modified:   __pycache__/print_file.cpython-311.pyc 	modified:   main.py 	modified:   menu.py 	modified:   parser.py 	modified:   print_file.py

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from menu import GPX_Menu
 from parser import parser
+from commands import cls
+import gpxpy
 
 def main():
 
-    print("Welcome to the GPX-editor!")
-    print("1 - Import GPX file")
-    print("0 - Exit")
-
     while True:
-        choice = input("Please select an option (1-0): ")
+        print("1 - Import GPX file")
+        print("2. Create new GPX file")
+        print("0 - Exit")
+        choice = input("Please select an option (1-2-0): ")
         if choice == '1':
             parsed_file = parser()
             GPX_Menu(parsed_file)
-
+        elif choice == '2':
+            new_file=gpxpy.gpx.GPX()
+            print("New GPX file created.")
+            GPX_Menu(new_file)
         elif choice == '0':
             print("Exiting the program.")
             break
 
         else:
             print("Invalid selection, please try again.")
+            cls()
+            continue
 
 main()
--- print_file.py
+++ print_file.py
@@ -1,11 +1,21 @@
+import pypdf
 
+def print_pdf(file_path):
+    # prints the saved gpx file to a PDF
+        pdf_writer = pypdf.PdfWriter()
+        pdf_reader = pypdf.PdfReader(file_path)
+
+        for page in pdf_reader.pages:
+            pdf_writer.add_page(page)
+
+        output_path = file_path.replace('.gpx', '.pdf').replace('.xml', '.pdf')
+        with open(output_path, 'wb') as output_pdf:
+            pdf_writer.write(output_pdf)
+
+        print(f"GPX file has been printed to PDF: {output_path}")
 
 def print_gpx(gpx_to_print):
-    """
-    Print the contents of a GPX file to the console.
-    
-    :param gpx_to_print: The GPX data to print.
-    """
-    
+  
+    # Print the contents of a GPX file to the console.
     print(gpx_to_print)
     print("\nEnd of file.")
